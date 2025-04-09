b54e7b8efaa8d1aed079f8e605b6c7ecf5b053f6
msaimanish <msaimanishreddy123@gmail.com>
2025-04-10 00:06:21 +0530

    hello

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n) {
-    if (n==0)
-        return 0;
-    if (n==1)
-        return 1;
-    return fib(n-1) + fib(n-2);
+
+int count_digits(int n){
+    int x = 0;
+    while (n > 0){
+        n/=10;
+        x++;
+    }
+    return x;
 }
 
-int main() {
+int power(int n, int m){
+
+    int x=1;
+    for (int i=0; i< m; i++){
+        x*=n;
+    }
+    return x;
+}
+
+
+int armstrong(int n){
+
+    int x=0;
+    int length = count_digits(n);
+    int n1 = n;
+    for (int i=0; i < length; i++){
+        x+=power(n1 % 10, length);
+        n1/=10;
+    }
+    return x==n;
+}
+
+
+int main(){
     int n;
+    cout << "Give a number: ";
     cin >> n;
-    cout << "Hello World!" << endl;
-    cout << fib(n);
+    if (armstrong(n)) cout << "It is an armstrong number" << endl;
+    else cout << "It is not an armstrong numver";
     return 0;
 }
